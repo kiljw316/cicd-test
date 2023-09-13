605f4f399075a8bb65b4b688443807942265eade
kiljaewon <kiljw316@gmail.com>
2023-09-13 23:04:49 +0900

    initial project

---
+++ src/main/kotlin/com/example/cicddemo/CicdDemoApplication.kt
@@ -0,0 +1,57 @@
+package com.example.cicddemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
+@SpringBootApplication
+class CicdDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<CicdDemoApplication>(*args)
+}
+
+@RestController
+class HealthController {
+    @GetMapping("/health")
+    fun health(): String = "OK"
+}
+
+@RestController
+class MessageController(val service: MessageService) {
+    @GetMapping("/")
+    fun index(): List<Message> = service.findMessages()
+
+    @GetMapping("/{id}")
+    fun index(@PathVariable id: String): List<Message> =
+        service.findMessageById(id)
+
+    @PostMapping("/")
+    fun post(@RequestBody message: Message) {
+        service.save(message)
+    }
+}
+
+@Service
+class MessageService(val db: MessageRepository) {
+    fun findMessages(): List<Message> = db.findAll().toList()
+
+    fun findMessageById(id: String): List<Message> = db.findById(id).toList()
+
+    fun save(message: Message) {
+        db.save(message)
+    }
+
+    fun <T : Any> Optional<out T>.toList(): List<T> =
+        if (isPresent) listOf(get()) else emptyList()
+}
+
+interface MessageRepository : CrudRepository<Message, String>
+
+@Table("MESSAGES")
+data class Message(@Id var id: String?, val text: String)
